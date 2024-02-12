43001d962909158e91cba0821a41a8146bd967b3
Umer04 <106334044+Umer04@users.noreply.github.com>
2024-02-12 21:56:16 +0500

    Header updated

---
+++ src/components/NavLinks.js
@@ -0,0 +1,11 @@
+// navLinks.js
+const NavLinks = [
+  { text: "Home", url: "#" },
+  { text: "About", url: "#" },
+  { text: "Services", url: "#" },
+  { text: "Projects", url: "#" },
+  { text: "Blogs", url: "#" },
+  { text: "Contact", url: "#" },
+];
+
+export default NavLinks;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import NavLinks from "./NavLinks";
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
@@ -8,26 +8,16 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
 
-  // Array of navigation links
-  const navLinks = [
-    { text: "Home", url: "#" },
-    { text: "About", url: "#" },
-    { text: "Services", url: "#" },
-    { text: "Projects", url: "#" },
-    { text: "Blogs", url: "#" },
-    { text: "Contact", url: "#" },
-  ];
-
   return (
-    <nav className="flex justify-between p-4 w-full relative">
+    <nav className="flex justify-between p-4 w-full fixed ">
       <div className="flex items-center">
         <a href="#" className="text-white text-2xl lg:text-3xl ml-2">
           Umer
         </a>
       </div>
-      <div className={`absolute md:relative top-full ml-4 md:top-0 md:left-auto  flex font-sans gap-4 italic font-semibold flex-col md:flex-row md:bg-transparent ${showMenu ? "flex" : "hidden md:flex"}`}>
-        {navLinks.map((link, index) => (
-          <a key={index} href={link.url} className="text-white transition duration-200 hover:opacity-75">
+      <div className={`absolute md:relative top-full  md:top-0 md:left-auto ${showMenu ? "bg-gray-900" : ""}  p-2 flex font-sans gap-4 italic font-semibold flex-col md:flex-row md:bg-transparent ${showMenu ? "flex" : "hidden md:flex"} "`}  >
+        {NavLinks.map((link, index) => (
+          <a key={index} href={link.url} className={`" text-white  px-1  transition duration-200 hover:opacity-50 "`}>
             {link.text}
           </a>
         ))}
